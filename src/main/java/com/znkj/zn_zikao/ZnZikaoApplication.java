5e79c38138b18dbf11c5b1dcd229b34cc9c8bb4d
FangCheng666 <1356712403@qq.com>
2019-04-15 02:03:25 +0800

    漏掉文件，再次提交

---
+++ src/main/java/com/znkj/zn_zikao/ZnZikaoApplication.java
@@ -0,0 +1,13 @@
+package com.znkj.zn_zikao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZnZikaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZnZikaoApplication.class, args);
+    }
+
+}
